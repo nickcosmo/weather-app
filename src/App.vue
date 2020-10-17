409e0defd019370897da04cca6278a213562df3c
Nick <30679225+nickcosmo@users.noreply.github.com>
2020-10-17 22:09:15 +0200

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Whats The Weather?</h1>
+  </div>
+  <div>
+    <input type="text" v-model="cityName">
+    <button @click='fetchData'>Get Weather!</button>
+  </div>
+  <weather v-bind:temperature="temperature"></weather>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      temperature: null,
+      cityName: '',
+    }
+  },
+  methods: {
+    fetchData() {
+      let temp = 0;
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=35ee102ebd52c46e68e293c39d139fe3`)
+        .then(res => res.json())
+        .then(weatherData => {temp = weatherData.main.temp
+           this.temperature = Math.floor(temp - 273.15);
+        });
+      return this.temperature;
+    }
+  },
   components: {
-    HelloWorld
+    Weather
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: black;
+  font-family: sans-serif;
+}
+
+div {
+  margin-top: 30px;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  font-size: 50px;
+}
+
+button {
+  background-color: white;
+  font-size: 20px;
+  padding: 5px 4px;
+  border: none;
+  font-family: Arial;
+  text-decoration: none;
+  display: inline-block;
+  color: black;
+  cursor: pointer;
+  outline: none;
+  margin-left: 5px;
+  /* transition: 0.5s ease-in-out; */
+}
+
+button:hover {
+  /* background-color: black;
+  color: white; */
+  animation: bump 0.2s ease-out;
+}
+
+input {
+  background-color: white;
+  padding: 5px 4px;
+  border: none;
+  font-family: Arial;
+  text-decoration: none;
+  display: inline-block;
+  color: black;
+  outline: none;
+  font-size: 20px;
+}
+
+.v-enter.active {
+
+}
+
+@keyframes bump {
+  0% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(-10px);
+  }
+
+  70% {
+    transform: translateY(+3px);
+  }
+
+  100% {
+    transform: translateY(0%);
+  }
 }
 </style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+    <transition mode="out-in">
+      <h2 v-if="temperature == null">...</h2>
+      <h2 v-else>{{ temperature }}&deg;</h2>
+    </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['temperature'],
+}
+</script>
+
+<style scoped>
+h2 {
+    margin-top: 50px;
+    font-size: 200px;
+}
+
+.v-enter-from, .v-leave-to {
+    opacity: 0;
+    transform: rotate(180deg);
+}
+
+.v-leave-to {
+    transform: rotate(360deg);
+}
+
+.v-enter-active {
+    transition: all 0.3s ease-out;
+}
+
+.v-leave-active {
+    transition: all 0.3s ease-in;
+}
+
+.v-enter-to, .v-leave-from {
+    opacity: 1;
+    transform: rotate(360deg);
+}
+</style>
